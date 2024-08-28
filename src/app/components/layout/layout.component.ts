d2fbd80d921eac2a551a6e4896277ce431aebf06
Marcelo Romero <romemarce1@gmail.com>
2024-08-28 20:07:10 -0300

    Optimize layout component

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,18 +1,21 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { initIProfile, IProfile } from 'src/types';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent  implements OnInit {
-  @Input() title: string = "test";
+export class LayoutComponent implements OnInit {
+  @Input() title: string = 'test';
 
   public isLoggedIn: boolean = false;
-  
-  ngOnInit() {
-    console.log('Title:',this.title)
-    console.log('IsLoggedIn:',this.isLoggedIn)
+
+  public profile: IProfile;
+
+  constructor(){
+    this.profile = { isLoggedIn: true }
   }
 
+  ngOnInit() { }
 }
--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
 import { LayoutComponent } from './layout.component';
+import { MenuComponent } from '../menu/menu.component';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, LayoutComponent],
-  declarations: [LayoutComponent],
+  imports: [CommonModule, IonicModule],
+  exports: [LayoutComponent],
+  declarations: [LayoutComponent, MenuComponent],
 })
-export class LayoutComponentModule {}
+export class LayoutModule {}

f2fc762fa66b558abd7888115201760f4cef2298
Marcelo Romero <romemarce1@gmail.com>
2024-08-28 21:37:26 -0300

    Some files

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,29 @@
 export interface IError {
-    isError: boolean,
-    title?: string,
-    code?: number,
-    message: string
+  isError: boolean;
+  title?: string;
+  code?: number;
+  message: string;
 }
 export const initIError: IError = {
-    isError: false,
-    message: ""
+  isError: false,
+  message: '',
+};
+
+export interface IProfile {
+  isLoggedIn: boolean;
+}
+
+export const initIProfile:IProfile = {
+  isLoggedIn: false,
+};
+
+export interface IMenuList {
+  path: string,
+  name: string,
+  icon?: string
+}
+export const initIMenuList:IMenuList = {
+  name: '',
+  path: '',
+  icon: ''
 }

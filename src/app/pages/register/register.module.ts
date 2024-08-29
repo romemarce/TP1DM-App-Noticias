e756985fd7bc3c2235240b1f985e3ec6e0da5486
Marcelo Romero <romemarce1@gmail.com>
2024-08-28 21:06:42 -0300

    Add page register

---
+++ src/app/pages/register/register.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { RegisterPageRoutingModule } from './register-routing.module';
+
+import { RegisterPage } from './register.page';
+import { LayoutModule } from 'src/app/components/layout/layout.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RegisterPageRoutingModule,
+    LayoutModule
+  ],
+  declarations: [RegisterPage]
+})
+export class RegisterPageModule {}

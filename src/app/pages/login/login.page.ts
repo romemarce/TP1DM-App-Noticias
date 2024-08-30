d11e29abc4d45d090171a2544f4ee891f1816453
Marcelo Romero <romemarce1@gmail.com>
2024-08-29 21:21:41 -0300

    Add login form

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +13,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
 
-  constructor() { }
+  user = new FormControl('');
+  password = new FormControl('');
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.loginForm = new FormBuilder().group({
+      user: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
   }
 
+  handleSubmit() {
+    // if (this.loginForm.valid) {
+      console.log({ user: this.user.value, password: this.password.value });
+    // }
+  }
 }

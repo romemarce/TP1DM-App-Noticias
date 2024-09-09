e9339cf6f8fd7e198dc677961ce45e0824ab78c4
Marcelo Romero <romemarce1@gmail.com>
2024-09-09 15:12:03 -0300

    Fix code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment.prod';
+import { enableProdMode } from '@angular/core';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+defineCustomElements(window);
+if (environment.production) {
+  enableProdMode();
+}
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
